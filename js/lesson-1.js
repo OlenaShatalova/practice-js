742f6d4d31ee41739ce651f15f201eddfe86ee3a
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2024-06-16 11:05:18 +0300

    add initial

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -5,7 +5,7 @@
 //то показати через alert: "Вірно!"
 //в противному випадку відобразити:"Не знаєте? ECMAScript!"
 
-const input = prompt("Яка офіційна назва JavaScript?");
+// const input = prompt("Яка офіційна назва JavaScript?");
 
 // if (input === "ECMAScript") {
 //   alert("Вірно!");
@@ -13,4 +13,18 @@ const input = prompt("Яка офіційна назва JavaScript?");
 //   alert("Не знаєте? ECMAScript!");
 // }
 
-alert(input === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!");
+// alert(input === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!");
+
+//Напишіть программу, яка отримує від користувача
+//число (кількість хвилин) и виводить у консоль
+//рядок у форматі годин та хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+const number = prompt("Enter number");
+const hour = 60;
+let hours = parseInt(number / hour);
+let minutes = number % hour;
+hours = String(hours).padStart(2, "0");
+minutes = String(minutes).padStart(2, "0");
+console.log(`${hours}:${minutes}`);
